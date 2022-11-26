c8020cba3998078bdccc921c40e9c28dad5de958
kumargogu <gksdesignengineer@gmail.com>
2022-11-26 17:15:53 +0530

    all operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { debounceTime, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'All-Operators';
+  res: any;
+  @ViewChild('name') ip: any;
+  ngAfterViewInit(): void{
+    fromEvent(this.ip.nativeElement, 'keyup').pipe(debounceTime(2000)).subscribe((x:any) => {
+      this.res = x.target.value;
+      
+    })
+  }
+  
 }
